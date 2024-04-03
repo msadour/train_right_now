783cbe4879b08dfdadaa34db9112de9e80104ebf
Mehdi <sadour.mehdi@gmail.com>
2024-04-03 03:14:25 +0200

    Add models.

--- app/endpoints/muscle/models.py
+++ app/endpoints/muscle/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Muscle(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class SubMuscle(models.Model):
+    label = models.CharField(max_length=255)
+    muscle = models.ForeignKey(Muscle, on_delete=models.CASCADE)
